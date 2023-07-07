5a9082047b27b26052f6036774dea5c2f5c4370c
ataman <atamanenkoalex90@gmail.com>
2023-07-07 15:02:50 +0300

    Add cache for books and authors views

--- book_store/settings.py
+++ book_store/settings.py
@@ -174,3 +174,10 @@ WHITENOISE_KEEP_ONLY_HASHED_FILES = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
