661e0cdb2f50f0144affdd1a66d791584cbbe984
ataman <atamanenkoalex90@gmail.com>
2023-07-10 02:01:36 +0300

    Debug

--- api_v2/views.py
+++ api_v2/views.py
@@ -158,6 +158,7 @@ def expire_view_cache(request, view_name, args=None, key_prefix=None):
         request_meta = {
                 "SERVER_NAME": request.get_host().split(":")[0],
                 # "SERVER_PORT": request.get_host().split(":")[1]
+                "SERVER_PORT": ""
             }
         # request_meta = {"SERVER_NAME": "0.0.0.0", "SERVER_PORT": "34515"}
 
